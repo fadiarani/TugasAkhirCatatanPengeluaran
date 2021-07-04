0e1ece1b30f9ec1c9379b85293f2588a43e40d7c
fadiarani <fadiarani9@gmail.com>
2021-07-04 19:44:08 +0700

    tugas akhir

---
+++ app/src/main/java/com/example/tugasakhir/create/CreateActivity.java
@@ -0,0 +1,77 @@
+package com.example.tugasakhir.create;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.app.catatpengeluaran.R;
+import com.app.catatpengeluaran.home.HomeActivity;
+import com.app.catatpengeluaran.utils.database.DaoHandler;
+import androidx.annotation.Nullable;
+
+import com.example.tugasakhir.R;
+import java.util.Objects;
+public class CreateActivity {
+
+    @BindView(R.id.etPembelian)
+    EditText etPembelian;
+    @BindView(R.id.etNominal)
+    EditText etNominal;
+    @BindView(R.id.btnSimpan)
+    Button btnSimpan;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Buat pengeluaran");
+
+        unbinder = ButterKnife.bind(this);
+        daoSession = DaoHandler.getInstance(this);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pembelian = etPembelian.getText().toString();
+                String nominal = etNominal.getText().toString();
+
+                if (pembelian.isEmpty() || nominal.isEmpty()){
+                    Toast.makeText(CreateActivity.this, "Data tidak boleh kosong", Toast.LENGTH_SHORT).show();
+                } else {
+                    /*
+                    Fungsi untuk menambahkan data kedalam database. Disini kita menambahkan data
+                    kedalam tabel TblPengeluaran.
+                    Di Greendao jika mau menambakan data fungsi yang kita panggil adalah insert.
+                     */
+                    TblPengeluaran tblPengeluaran = new TblPengeluaran();
+                    tblPengeluaran.setPengeluaran(pembelian);
+                    tblPengeluaran.setNominal(Integer.parseInt(nominal));
+                    daoSession.getTblPengeluaranDao().insert(tblPengeluaran);
+
+                    Toast.makeText(CreateActivity.this, "Berhasil menginput data",
+                            Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(CreateActivity.this, HomeActivity.class)
+                            .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP));
+                    finish();
+                }
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+}
---
+++ app/src/main/java/com/example/tugasakhir/utils/DaoHandler.java
@@ -0,0 +1,18 @@
+package com.example.tugasakhir.utils;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+public class DaoHandler {
+
+    /*
+    Fungsi untuk konfigurasi Greendao. Dibuat singleton class agar tinggal panggil saja.
+     */
+    public static DaoSession getInstance(Context context) {
+        DaoMaster.DevOpenHelper helper = new DaoMaster.DevOpenHelper(context, "catatpengeluaran_db", null);
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        DaoMaster daoMaster = new DaoMaster(db);
+        return daoMaster.newSession();
+    }
+}
